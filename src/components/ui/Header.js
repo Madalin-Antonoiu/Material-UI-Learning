e3305803970d68c7a7bea006c9d62135d817548d
mantonoiu <antonoiumadalin@gmail.com>
2021-03-18 10:46:16 +0000

    Active Tab Refresh Fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, Tabs, Tab, Button } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from "@material-ui/core/styles"
@@ -50,6 +50,25 @@ export default function Header(props) {
     const handleChange = (e, value) => {
         setValue(value);
     }
+    const activeTabRefreshFix = (value) => {
+        if (window.location.pathname === "/" && value !== 0) {
+            setValue(0)
+        } else if (window.location.pathname === "/services" && value !== 1) {
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2) {
+            setValue(2)
+        } else if (window.location.pathname === "/about" && value !== 3) {
+            setValue(3)
+        } else if (window.location.pathname === "/contact" && value !== 4) {
+            setValue(4)
+        } else if (window.location.pathname === "/estimate" && value !== 5) {
+            setValue(5)
+        }
+    }
+
+    useEffect(() => {
+        activeTabRefreshFix(value);
+    }, [value])
 
     return <>
         <ElevationScroll>
