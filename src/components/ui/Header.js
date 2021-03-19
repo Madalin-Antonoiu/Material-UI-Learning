0d1ffaf1898064350d55293a541ba34036d01e39
mantonoiu <antonoiumadalin@gmail.com>
2021-03-19 09:14:34 +0000

    Menus - Refresh Fix and Refactor to switch() statement

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -93,6 +93,29 @@ export default function Header(props) {
     ]
 
     const activeTabRefreshFix = (value) => {
+        switch (window.location.pathname) {
+            default:
+                break;
+            case "/":
+                if (value !== 0) { setValue(0) } break;
+            case "/services":
+                if (value !== 1) { setValue(1); setSelectedIndex(0); } break;
+            case "/customsoftware":
+                if (value !== 1) { setValue(1); setSelectedIndex(1); } break;
+            case "/mobileapps":
+                if (value !== 1) { setValue(1); setSelectedIndex(2); } break;
+            case "/websites":
+                if (value !== 1) { setValue(1); setSelectedIndex(3); } break;
+            case "/revolution":
+                if (value !== 2) { setValue(2); } break;
+            case "/about":
+                if (value !== 3) { setValue(3); } break;
+            case "/contact":
+                if (value !== 4) { setValue(4); } break;
+            case "/estimate":
+                if (value !== 5) { setValue(5); } break;
+        }
+
         if (window.location.pathname === "/" && value !== 0) {
             setValue(0)
         } else if (window.location.pathname === "/services" && value !== 1) {
@@ -107,6 +130,7 @@ export default function Header(props) {
             setValue(5)
         }
     }
+
     useEffect(() => {
         activeTabRefreshFix(value);
     }, [value])
